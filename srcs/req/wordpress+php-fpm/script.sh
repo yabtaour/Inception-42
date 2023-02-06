0010859db1bf31d27c105491d7196a4f13664c5e
yabtaour <younessabtaouri00@gmail.com>
2023-02-06 12:33:38 -0600

    all good

--- srcs/req/wordpress+php-fpm/script.sh
+++ srcs/req/wordpress+php-fpm/script.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+mkdir -p /var/www/html
+
+chmod 777 /var/www/html
+
+mkdir -p /run/php
 
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
-    chmod +x wp-cli.phar
-    mv wp-cli.phar /usr/local/bin/wp
-    wp core download --allow-root
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+wp core download  --path=/var/www/html --allow-root
 
-wp core install --allow-root --url="https://yabtaour.42.fr" --title="inception" --admin_user="yabtaour" --admin_password="yabtaour69" --admin_email="yabtaour@student.1337.ma" --skip-email
+wp config create --dbname=mydb --dbuser=db_master --dbpass=youness69 --dbhost=mariadb --path=/var/www/html --allow-root --skip-check
 
-# wp config create --dbname=mydb --dbuser=db_master --dbpass=youness69 --dbhost=mariadb --path=/usr/share/wordpress --allow-root --skip-check --extra-php
+wp core install --path=/var/www/html  --allow-root --url="https://yabtaour.42.fr" --title="inception" --admin_user="yabtaour" --admin_password="yabtaour69" --admin_email="yabtaour@student.1337.ma" --skip-email
 
-wp user create youness youness@gmail.com  --user_pass=youness69 --role=author --allow-root
+wp user create youness youness@gmail.com  --user_pass=youness69 --role=author --path=/var/www/html --allow-root
 
-wp theme install Zeever --activate --allow-root
+# wp theme install Zeever --activate --path=/var/www/html --allow-root
 
-# php-fpm7.3 --nodaemonize
+php-fpm7.3 --nodaemonize
