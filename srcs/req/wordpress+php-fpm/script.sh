0648ec945c7b63ef8303a038304640ebd6583615
yabtaour <younessabtaouri00@gmail.com>
2023-02-15 01:30:38 -0600

    all woking had sa3a

--- srcs/req/wordpress+php-fpm/script.sh
+++ srcs/req/wordpress+php-fpm/script.sh
@@ -2,8 +2,9 @@
 mkdir -p /var/www/html
 
 #chown -R www-data:www-data /var/www/html/
-#groupadd www-pub
-#	usermod -aG www-pub www-data
+groupadd www-pub
+usermod -aG www-pub www-data
+
 chmod 777 /var/www/html
 
 mkdir -p /run/php
@@ -13,13 +14,14 @@ wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp core download  --path=/var/www/html --allow-root
-groupadd 
-chown -R www-data:www-data /var/www/html
+#groupadd 
 wp config create --dbname=mydb --dbuser=db_master --dbpass=youness69 --dbhost=mariadb --path=/var/www/html --allow-root --skip-check --extra-php <<PHP
 define('WP_REDIS_HOST', 'redis');
 define('WP_REDIS_PORT', 6379);
 define('WP_REDIS_DISABLED', false);
 PHP
+
+chown -R www-pub /var/www/html
 
 wp core install --path=/var/www/html  --allow-root --url="https://yabtaour.42.fr" --title="inception" --admin_user="yabtaour" --admin_password="yabtaour69" --admin_email="yabtaour@student.1337.ma" --skip-email
 
