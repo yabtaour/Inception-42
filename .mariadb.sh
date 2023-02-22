7ce0582fbf276bdf95fc5a7da364cd9e61e1af06
yabtaour <younessabtaouri00@gmail.com>
2023-02-22 10:00:53 -0500

    ftp and redis are working

--- .mariadb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "CREATE DATABASE IF NOT EXISTS mydb;" >srcs/requirements/mariadb/conf/db.sql
-echo "CREATE USER 'db_master' IDENTIFIED BY 'youness69';" >> srcs/requirements/mariadb/conf/db.sql
-echo "GRANT ALL PRIVILEGES ON mydb.* TO 'db_master';" >> srcs/requirements/mariadb/conf/db.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root69';" >> srcs/requirements/mariadb/conf/db.sql
-echo "FLUSH PRIVILEGES" >> srcs/requirements/mariadb/conf/db.sql;
-
--- srcs/requirements/wordpress+php-fpm/tools/script.sh
+++ srcs/requirements/wordpress+php-fpm/tools/script.sh
@@ -14,22 +14,27 @@ wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp core download  --path=/var/www/html --allow-root
-#groupadd 
+#groupadd
+chown -R www-data:www-data /var/www/html
 wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --path=/var/www/html --allow-root --skip-check --extra-php <<PHP
 define('WP_REDIS_HOST', 'redis');
 define('WP_REDIS_PORT', 6379);
 define('WP_REDIS_DISABLED', false);
 PHP
 
-chown -R :www-pub /var/www/html
+#chown -R :www-pub /var/www/html
 
 wp core install --path=/var/www/html  --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_PASSWORD --admin_email=$WP_ADMIN_EMAIL  --skip-email
 
 wp user create $WP_USER $WP_USER_EMAIL  --user_pass=$WP_USER_PASSWORD --role=author --path=/var/www/html --allow-root
 
+service php7.3-fpm start
+
 wp plugin install redis-cache --path=/var/www/html --allow-root
 wp plugin activate redis-cache --path=/var/www/html --allow-root
 wp redis enable --path=/var/www/html --allow-root
+
+service php7.3-fpm stop
 
 #wp theme install zeever --activate --path=/var/www/html --allow-root
 
