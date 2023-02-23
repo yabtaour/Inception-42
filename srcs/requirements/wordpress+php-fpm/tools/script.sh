3ee46f269e93f318941c5887b09bb1fdb42164a5
yabtaour <yabtaour@debian.yabtaour.42.fr>
2023-02-22 22:51:26 -0500

    env file aded

--- srcs/requirements/wordpress+php-fpm/tools/script.sh
+++ srcs/requirements/wordpress+php-fpm/tools/script.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 mkdir -p /var/www/html
 
-#chown -R www-data:www-data /var/www/html/
-groupadd www-pub
-usermod -aG www-pub www-data
-
 chmod 777 /var/www/html
 
 mkdir -p /run/php
@@ -14,17 +10,17 @@ wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp core download  --path=/var/www/html --allow-root
-#groupadd
+
 chown -R www-data:www-data /var/www/html
-wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --path=/var/www/html --allow-root --skip-check --extra-php <<PHP
+
+wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_USER_PASSWORD --dbhost=$MARIADB_HOST --path=/var/www/html --allow-root --skip-check --extra-php <<PHP
 define('WP_REDIS_HOST', 'redis');
 define('WP_REDIS_PORT', 6379);
 define('WP_REDIS_DISABLED', false);
 PHP
 
-#chown -R :www-pub /var/www/html
 
-wp core install --path=/var/www/html  --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_PASSWORD --admin_email=$WP_ADMIN_EMAIL  --skip-email
+wp core install --path=/var/www/html  --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL  --skip-email
 
 wp user create $WP_USER $WP_USER_EMAIL  --user_pass=$WP_USER_PASSWORD --role=author --path=/var/www/html --allow-root
 
